87253c02041bd37e0b9967fa32d3afd17e45a193
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-02 16:15:33 +0600

    add snackBar

---
+++ lib/utils/utils.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class Utils {
+  static snackBar(String title, String message) {
+    Get.snackbar(
+      title,
+      message,
+    );
+  }
+}
